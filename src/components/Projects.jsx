2471d02f300489748bd159ea4d5256a19e8adb78
uyenlai <98347695+uyenlai@users.noreply.github.com>
2024-08-10 02:04:18 +0300

    add real projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,29 +7,74 @@ import Tabs from "react-bootstrap/esm/Tabs";
 import TabContainer from "react-bootstrap/esm/TabContainer";
 import TabContent from "react-bootstrap/esm/TabContent";
 import ProjectCard from "./ProjectCard";
-import Project1 from "../assets/img/project-img1.png";
-import Project2 from "../assets/img/project-img2.png";
-import Project3 from "../assets/img/project-img3.png";
+import Project1 from "../assets/img/food-order-app.png";
+import Project2 from "../assets/img/hover-effect-btn.png";
+import Project3 from "../assets/img/tab-navigation.png";
+import Project4 from "../assets/img/calculator-app.png";
+import Project5 from "../assets/img/events-page.png";
+import Project6 from "../assets/img/guess-the-number.png";
+import Project7 from "../assets/img/todo-list.png";
+import Project8 from "../assets/img/quiz-game.png";
 
 const Projects = () => {
-  const projects = [
+  const tab1 = [
     {
-      href: "https://github.com/uyenlai?tab=repositories",
+      href: "https://github.com/uyenlai/food-order-app",
       imgUrl: Project1,
-      title: "Project 1",
-      description: "Lorem...",
+      title: "Food Order App",
+      description:
+        "A dynamic React food order app, integrating API data to populate the menu. Managed with useContext, useReducer, useState, and useEffect hooks for efficient state handling.",
     },
     {
-      href: "https://github.com/uyenlai?tab=repositories",
+      href: "https://github.com/uyenlai/button-ripple-effect",
       imgUrl: Project2,
-      title: "Project 2",
-      description: "Lorem...",
+      title: "Ripple Effect Button",
+      description:
+        "A captivating Ripple Effect Button in Vanilla JavaScript that fills with color from the hover point, creating a dynamic visual experience.",
     },
     {
-      href: "https://github.com/uyenlai?tab=repositories",
+      href: "https://github.com/uyenlai/tabs-navigation",
       imgUrl: Project3,
-      title: "Project 3",
-      description: "Lorem...",
+      title: "Tabs Navigation",
+      description:
+        "A Tabs Navigation system in pure JavaScript, where clicking a tab updates the active class and dynamically displays the relevant content.",
+    },
+    {
+      href: "https://github.com/uyenlai/calculator-app",
+      imgUrl: Project4,
+      title: "Calculator",
+      description:
+        "A simple, user-friendly calculator built with pure JavaScript, HTML, and CSS.",
+    },
+  ];
+
+  const tab2 = [
+    {
+      href: "https://github.com/uyenlai/events-page-with-firebase",
+      imgUrl: Project5,
+      title: "Events Page",
+      description:
+        "Easily manage events on our page. Sign up or log in to create, edit, or delete events with ease. Powered by Redux Toolkit, React Router, and Firebase for smooth authentication and data storage.",
+    },
+    {
+      href: "https://github.com/uyenlai/guess-the-number",
+      imgUrl: Project6,
+      title: "Guess The Number",
+      description:
+        'A Vanilla JavaScript "Guess the Number" game: Guess a number between 1 and 20, get feedback, and play again after guessing correctly.',
+    },
+    {
+      href: "https://github.com/uyenlai/todo-list",
+      imgUrl: Project7,
+      title: "To-do List",
+      description:
+        "A React-powered To-Do List app where users can easily create, update statuses, and delete tasks for efficient management.",
+    },
+    {
+      href: "https://github.com/uyenlai/quiz-app",
+      imgUrl: Project8,
+      title: "Quiz game",
+      description: "A simple quiz game built with Typescript",
     },
   ];
   const [activeTab, setActiveTab] = useState("tab1");
@@ -57,7 +102,7 @@ const Projects = () => {
                 <Tab eventKey="tab1" title="Tab 1">
                   <TabContent>
                     <Row>
-                      {projects.map((project, index) => (
+                      {tab1.map((project, index) => (
                         <ProjectCard
                           key={index}
                           href={project.href}
@@ -72,14 +117,15 @@ const Projects = () => {
                 <Tab eventKey="tab2" title="Tab 2">
                   <TabContent>
                     <Row>
-                      {/* {projects.map((project) => (
+                      {tab2.map((project, index) => (
                         <ProjectCard
-                          key={project}
+                          key={index}
+                          href={project.href}
                           imgUrl={project.imgUrl}
                           title={project.title}
                           description={project.description}
                         />
-                      ))} */}
+                      ))}
                     </Row>
                   </TabContent>
                 </Tab>
